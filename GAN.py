58d616d94c1a524440438413ad2c99681ee639cf
John <johnathangendron@gmail.com>
2019-03-31 08:58:31 -0400

    Update GAN.py
    save checkpoint every 50epoch and save imgs every 5 of em

--- GAN.py
+++ GAN.py
@@ -234,11 +234,11 @@ def train():
             # print 'train:[%d/%d],d_loss:%f,g_loss:%f' % (i, j, dLoss, gLoss)
             
         # save check point every 500 epoch
-        if i%500 == 0:
+        if i%50 == 0:
             if not os.path.exists('./model/' + version):
                 os.makedirs('./model/' + version)
             saver.save(sess, './model/' +version + '/' + str(i))  
-        if i%50 == 0:
+        if i%5 == 0:
             # save images
             if not os.path.exists(newPoke_path):
                 os.makedirs(newPoke_path)
